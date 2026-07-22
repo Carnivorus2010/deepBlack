7d3c10e4ff8c620e2397cc2fb645f94fda94ae5d
Carnivorus2010 <travwied@gmail.com>
2026-07-21 20:02:37 -0500

    Add battery status to dwl bar

--- scripts/status.sh
+++ scripts/status.sh
@@ -5,7 +5,32 @@ if command -v slstatus >/dev/null 2>&1; then
     exec slstatus -s
 fi
 
+cd /sys/class/power_supply/BAT0
+
 while :; do
-    date '+%a %Y-%m-%d %H:%M'
-    sleep 30
+	if [ -r capacity ]; then
+		battery_capacity=$(cat capacity)
+		battery_status=$(cat status)
+
+		case "$battery_status" in
+			Charging)
+				battery_icon=" 󰂄"
+				;;
+			Full)
+				battery_icon=" 󰁹"
+				;;
+			*)
+				battery_icon=" 󰁿"
+				;;
+		esac
+
+		printf '%s %s%% | %s\n' \
+			"$battery_icon" \
+			"$battery_capacity" \
+			"$(date '+%a %Y-%m-%d %H:%M')"
+	else
+		date '+%a %Y-%m-%d %H:%M'
+	fi
+
+	sleep 30
 done
